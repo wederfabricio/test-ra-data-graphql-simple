fda0a3156acab3b11fc324cc136959fb72133043
Mathieu <mathieu@eyaka.be>
2020-10-15 16:17:33 +0200

    installation of react-admin and tutorial

---
+++ src/posts.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    SimpleForm,
+    TextInput,
+    Edit,
+    ReferenceInput,
+    SelectInput,
+    Create
+} from 'react-admin';
+
+const PostTitle = ({record}) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostList = props => (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="id"/>
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name"/>
+            </ReferenceField>
+            <TextField source="id"/>
+            <TextField source="title"/>
+            <TextField source="body"/>
+            <EditButton/>
+        </Datagrid>
+    </List>
+);
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id"/>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name"/>
+            </ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name"/>
+            </ReferenceInput>
+            <TextInput source="title"/>
+            <TextInput multiline source="body"/>
+        </SimpleForm>
+    </Create>
+);
